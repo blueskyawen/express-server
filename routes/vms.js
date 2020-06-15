d78717b379be6b95317eb1a88d8595fb98caa624
blueskyawen <bluesky.liu@163.com>
2020-06-16 01:43:33 +0800

    add4

--- routes/vms.js
+++ routes/vms.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
     mongoClient.connect(function(err, db) {
         if (err) throw err;
         var addVm = {id: "001", name: "my-vm", status: "active"};
-        db.db(dbName).collection("vms").insertOne(addVm, function (err, res) {
+        db.db(dbName).collection("vms").insertOne(addVm, function (err, res2) {
             if (err) throw err;
             console.log("文档插入成功");
             db.close();
