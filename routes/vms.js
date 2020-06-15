26f63cd486fd207e27bd411f1936e2a70c2c53d6
10196470 <wang.ping6@zte.com.cn>
2020-06-15 20:09:36 +0800

    add mongodbd

--- routes/vms.js
+++ routes/vms.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/vms";
+var mongoClient = new MongoClient(url,{ useNewUrlParser: true });
 
 router.get('/', function(req, res, next) {
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    mongoClient.connect(function(err, db) {
         if (err) throw err;
         console.log("vms-数据库已创建!");
         db.close();
