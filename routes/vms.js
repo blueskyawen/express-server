b76dd8d9c390a7858337d3cf1b7ff31ec59538e6
blueskyawen <bluesky.liu@163.com>
2020-06-16 01:38:19 +0800

    add

--- routes/vms.js
+++ routes/vms.js
@@ -3,14 +3,23 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/vms";
 var mongoClient = new MongoClient(url,{ useNewUrlParser: true, useUnifiedTopology: true });
+var Mock = require('mockjs');
+
+const dbName = "cloudenv";
 
 router.get('/', function(req, res, next) {
     mongoClient.connect(function(err, db) {
         if (err) throw err;
-        console.log("vms-数据库已创建!");
-        db.close();
-        res.end('vms-数据库已创建!');
+        var addVm = {id: Mock.id(), name: "my-vm", status: "active"};
+        client.db(dbName).collection("vms").insertOne(addVm, function (err, res) {
+            if (err) throw err;
+            console.log("文档插入成功");
+            db.close();
+            res.send(addVm);
+        });
     });
 });
 
+
 module.exports = router;
+
