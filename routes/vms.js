e19788a8cf51e34c0d00658bd81bd0d12a283c7c
blueskyawen <bluesky.liu@163.com>
2020-06-16 17:02:59 +0800

    fix

--- routes/vms.js
+++ routes/vms.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/vms";
+var url = "mongodb://localhost:27017";
 var mongoClient = new MongoClient(url,{ useNewUrlParser: true, useUnifiedTopology: true });
 var Mock = require('mockjs');
 
 const dbName = "cloudenv";
 
 router.get('/', function(req, res, next) {
-    mongoClient.connect(function(err, db) {
+    MongoClient.connect(url,{ useNewUrlParser: true},function(err, db) {
         if (err) throw err;
         db.db(dbName).collection("vms").find({}).toArray(function (err, result) {
             if (err) throw err;
@@ -52,7 +52,7 @@ router.delete('/:id', function(req, res, next) {
 });
 
 router.get('/:id', function(req, res, next) {
-    mongoClient.connect(function(err, db) {
+    MongoClient.connect(url,{ useNewUrlParser: true},function(err, db) {
         if (err) throw err;
         db.db(dbName).collection("vms").find({id: req.params.id}).limit(1).toArray(function (err, result) {
             if (err) throw err;
